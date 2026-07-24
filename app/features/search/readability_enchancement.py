65afa9c2cfa8053b2b0f6c338d2930026f85d91f
Masterravi2000 <raviqazqwerty123456@gmail.com>
2026-07-25 02:59:16 +0530

    FEATURE - readability_enchancement layer  implemented and tested successfully

--- app/features/search/readability_enchancement.py
+++ app/features/search/readability_enchancement.py
@@ -7,7 +7,28 @@ def enchance_readability(query: str, results: List[dict]) -> List[dict]:
     """
     enchanced_results = []
     
-    # for result in results:
+    for result in results:
+        content = result["content"]
+        
+        # Find the starting index of the query
+        start_index = content.lower().find(query.lower())
+        
+        # if not found then keep the original content
+        if start_index == -1:
+            result["content"] = content
+            enchanced_results.append(result)
+            continue
+        
+        # 50 characters before and after the match
+        snippet_start = max(0, start_index - 50)
+        snippet_end = min(len(content), start_index + len(query) + 50)
+        
+        snippet = content[snippet_start:snippet_end]
+        
+        # Replace full content with snippet
+        result["content"] = snippet
+        
+        enchanced_results.append(result)
         
     
     return enchanced_results
--- app/features/search/search.py
+++ app/features/search/search.py
@@ -2,6 +2,7 @@ from app.features.embedding.embedder import model
 from app.repositories.lancedb_repository import table
 from app.features.search.hybrid_search import hybrid_filter
 from app.features.search.ranking import rank_results
+from app.features.search.readability_enchancement import enchance_readability
 
 
 def search_query(query: str, k: int = 5):
@@ -36,5 +37,6 @@ def search_query(query: str, k: int = 5):
     # Step 4: Hybrid Search
     filtered_result = hybrid_filter(query, formatted_results)
     filtered_result = rank_results(query, filtered_result)
+    filtered_result = enchance_readability(query, filtered_result)
     
     return filtered_result
